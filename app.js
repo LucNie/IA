21955709665df6772d653fcf5f3e605629fac00e
Galaxial <90603307+LucNie@users.noreply.github.com>
2023-03-02 12:27:03 +0100

    V1 work

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const brain = require('brain.js');
-const net = new brain.NeuralNetwork( { hiddenLayers: [300] } );
+const net = new brain.NeuralNetwork({ hiddenLayers: [200] });
 const fs = require('fs');
 const { normalize } = require('path');
 const path = require('path');
@@ -7,7 +7,19 @@ const path = require('path');
 const pathMnist = path.join(__dirname, './data/mnist_test.csv');
 const mnist = fs.readFileSync(pathMnist, 'utf8').split('\r').map(row => row.split(',').map(Number));
 
-/**
+const config = {
+    iterations: 300,
+    // errorThresh: 0.005,
+    log: true,
+    logPeriod: 10,
+    learningRate: 0.3,
+    // momentum: 0.1,
+    // callback: null,
+    // callbackPeriod: 10,
+    // timeout: Infinity
+};
+
+/** NOTE * Lucas 
  * mnist is a 28x28 image of a number from 0-9
  * the first column is the number the image represents
  * the rest of the columns are the pixel values
@@ -16,9 +28,11 @@ const mnist = fs.readFileSync(pathMnist, 'utf8').split('\r').map(row => row.spli
 const normalizemnist = mnist.map(row => {
     return {
         input: row.slice(1) / 255,
-        output: [row[0] / 9] 
+        output: [row[0] / 9]
     }
 });
+
+// console.log("done normalizing", normalizemnist[0].input);
 
 const normalizemnist2 = mnist.map(row => { // set > 127 to 1, < 127 to 0
     return {
@@ -27,30 +41,24 @@ const normalizemnist2 = mnist.map(row => { // set > 127 to 1, < 127 to 0
     }
 });
 
+// console.log("done normalizing2", normalizemnist2[0].input);
 
-const config = {
-    iterations: 100,
-    // errorThresh: 0.005,
-    log: true,
-    // logPeriod: 10,
-    learningRate: 0.3,
-    // momentum: 0.1,
-    // callback: null,
-    // callbackPeriod: 10,
-    // timeout: Infinity
-};
-// train with gpu
+
+
+// train the network
 net.train(normalizemnist2, config);
 
 
 // test the network
 const test = mnist.slice(0, 100).map(row => { // test the first 100 rows
     return {
-        input: row.slice(1) / 255,
+        input: row.slice(1).map(x => x > 127 ? 1 : 0),
         output: [row[0] / 9]
     }
 });
 
-
-const output = net.run(test[99].input);
-console.log("test output: ", output * 9 , "expected: ", test[99].output * 9);
+for (let i = 0; i < 5; i++) {
+    let _rand = Math.round(Math.random() * 100);
+    const output = Math.round(net.run(test[_rand].input));
+    console.log("data : " + _rand," test output: ", output * 9, "expected: ", test[_rand].output * 9);
+}
